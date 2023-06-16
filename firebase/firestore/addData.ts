8dc39ff2dc1919af88f9743008e6b9ae7539ee87
root <root@vmi1309140.contaboserver.net>
2023-06-16 08:26:18 +0200

    update

--- firebase/firestore/addData.ts
+++ firebase/firestore/addData.ts
@@ -2,7 +2,7 @@ import firebase_app from "../config";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const db = getFirestore(firebase_app)
-export default async function addData(colllection, id, data) {
+export default async function addData(colllection:any, id:any, data:any) {
     let result = null;
     let error = null;
 
